5a898b48c30c5b7285c04f63d39371c61ccf24c7
Jonathan Tissot <jonathan.tissot@gmail.com>
2026-02-08 19:56:53 -0300

    eks cluster

--- main.tf
+++ main.tf
@@ -1,5 +1,44 @@
-module "networking" {
-  source = "./modules/networking"
-  vpc = var.vpc
-  environment = var.environment
+module "aws_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "= 6.6.0"
+
+  name = "test-vpc"
+  cidr = var.vpc.cidr_block
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets = var.vpc.private_subnets
+  public_subnets  = var.vpc.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = var.environment
+  }
+}
+
+module "aws_eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "= 21.15.1"
+
+  name               = "test-eks"
+  kubernetes_version = "1.34"
+
+  endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+  # aws_vpc.main.id
+  vpc_id     = module.aws_vpc.vpc_id
+  subnet_ids = module.aws_vpc.private_subnets
+
+  tags = {
+    Environment = var.environment
+    Terraform   = "true"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "vpc" {
   type = object({
     cidr_block = string
-    public_subnets = number
-    private_subnets = number
+    public_subnets = list(string)
+    private_subnets = list(string)
   })
 }
 
